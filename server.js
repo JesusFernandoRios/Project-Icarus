a84f71cb559d76407b33ec0966caf418de0f6133
DarkDave <darkdave1185@gmail.com>
2020-12-03 16:47:15 -0700

    another server.js change

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(routes);
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  path.join(express.static(__dirname, "client/build"));
+  app.use(express.static(__dirname, "client/build/index.html"));
 }
 
 // Send every request to the React app
