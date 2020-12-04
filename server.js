c61abc425c5124262d28bbcb515ded5ce674e036
DarkDave <darkdave1185@gmail.com>
2020-12-03 17:47:20 -0700

    a few adjustments

--- server.js
+++ server.js
@@ -15,13 +15,13 @@ app.use(routes);
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(__dirname, "client/build/index.html"));
+  app.use(express.static(__dirname, "/client/build/index.html"));
 }
 
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "/client/build"));
 });
 
 app.listen(PORT, function () {
